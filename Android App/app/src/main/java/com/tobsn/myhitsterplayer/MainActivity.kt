6c6e86ee1cece621e20d6b0dc1ce8516781eb64c
tobsn09 <t.pohl91@web.de>
2026-02-22 00:19:36 +0100

    update config und reset button

--- Android App/app/src/main/java/com/tobsn/myhitsterplayer/MainActivity.kt
+++ Android App/app/src/main/java/com/tobsn/myhitsterplayer/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.WindowInsets
 import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
-import com.google.mlkit.vision.barcode.common.Barcode
-import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
-import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myWebView: WebView
@@ -19,35 +16,26 @@ class MainActivity : AppCompatActivity() {
         window.insetsController?.hide(WindowInsets.Type.statusBars())
 
         myWebView = findViewById(R.id.webview)
-        val settings = myWebView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-
-        // S24 Ultra Tarnung
-        settings.userAgentString = "Mozilla/5.0 (Linux; Android 14; SM-S928B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36"
-
-        myWebView.addJavascriptInterface(WebAppInterface(), "AndroidBridge")
-
-        // WICHTIG: Erzwingt das Neuladen nach Redirects
-        myWebView.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                if (url != null && url.contains("#access_token")) {
-                    view?.evaluateJavascript("checkForToken();", null)
-                }
-            }
-        }
+        setupWebView()
         myWebView.loadUrl("https://tobsn09.github.io/hitster_disney_ger/")
     }
 
-    inner class WebAppInterface {
-        @JavascriptInterface
-        fun startScanner() {
-            val options = GmsBarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build()
-            GmsBarcodeScanning.getClient(this@MainActivity, options).startScan()
-                .addOnSuccessListener { barcode ->
-                    barcode.rawValue?.let { if (it.contains("github.io")) runOnUiThread { myWebView.loadUrl(it) } }
+    private fun setupWebView() {
+        val settings = myWebView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.databaseEnabled = true
+        settings.userAgentString = "Mozilla/5.0 (Linux; Android 14; SM-S928B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36"
+
+        myWebView.webViewClient = object : WebViewClient() {
+            // Dies triggert bei JEDEM Laden, auch nach dem Spotify-Redirect
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                if (url != null && url.contains("#access_token")) {
+                    // Wir schubsen das JavaScript manuell an
+                    view?.evaluateJavascript("checkForToken();", null)
                 }
+            }
         }
     }
 }
