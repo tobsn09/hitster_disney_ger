cf38dc057de404fd88dc78906f3beb2c0e6fea06
tobsn09 <t.pohl91@web.de>
2026-02-21 23:31:31 +0100

    update config und reset button

--- Android App/app/src/main/java/com/tobsn/myhitsterplayer/MainActivity.kt
+++ Android App/app/src/main/java/com/tobsn/myhitsterplayer/MainActivity.kt
@@ -2,7 +2,6 @@ package com.tobsn.myhitsterplayer
 
 import android.os.Bundle
 import android.view.WindowInsets
-import android.view.WindowInsetsController
 import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.mlkit.vision.barcode.common.Barcode
@@ -16,31 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Fullscreen Modus
         window.setDecorFitsSystemWindows(false)
         window.insetsController?.hide(WindowInsets.Type.statusBars())
 
         myWebView = findViewById(R.id.webview)
-        setupWebView()
-
-        myWebView.addJavascriptInterface(WebAppInterface(), "AndroidBridge")
-        myWebView.loadUrl("https://tobsn09.github.io/hitster_disney_ger/")
-    }
-
-    private fun setupWebView() {
         val settings = myWebView.settings
         settings.javaScriptEnabled = true
         settings.domStorageEnabled = true
         settings.databaseEnabled = true
         settings.mediaPlaybackRequiresUserGesture = false
 
-        // DIESE ZEILE TARNT DIE APP ALS CHROME BROWSER (Wichtig für Spotify Login!)
-        settings.userAgentString = "Mozilla/5.0 (Linux; Android 13; SM-S928B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
+        // S24 Ultra Chrome-Identität für stabilere Logins
+        settings.userAgentString = "Mozilla/5.0 (Linux; Android 14; SM-S928B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36"
 
-        myWebView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                return false
-            }
-        }
+        myWebView.addJavascriptInterface(WebAppInterface(), "AndroidBridge")
+        myWebView.webViewClient = WebViewClient()
+        myWebView.loadUrl("https://tobsn09.github.io/hitster_disney_ger/")
     }
 
     inner class WebAppInterface {
